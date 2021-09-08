4daa335a337f888622fdc21cdf149557aa99cae6
nicholasnge <59093518+nicholasnge@users.noreply.github.com>
2021-09-08 14:48:57 +0800

    added crafting table

---
+++ Team24/Code24/source/CraftingBench.cpp
@@ -0,0 +1,13 @@
+
+#include "CraftingBench.h"
+
+
+
+void CraftingBench::extractDesigns(shared_ptr<Program> program) {
+
+	vector<shared_ptr<Procedure>> procedures = program->getProcedures();
+	for (shared_ptr<Procedure> procedure : procedures) {
+		PKBStatement
+	}
+
+}
---
+++ Team24/Code24/source/CraftingBench.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "SimpleAST.h"
+#include "PKBStatement.h"
+
+class CraftingBench {
+
+public:
+
+
+	void extractDesigns(shared_ptr<Program> program);
+
+protected:
+
+};
