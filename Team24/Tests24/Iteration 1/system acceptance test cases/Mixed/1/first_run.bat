0972c6a649ef53d7a8da384a79036f4736adb139
muhd97 <zain.sma.mz@gmail.com>
2021-09-17 21:02:57 +0800

    Add scripts to run individual tests

--- Team24/Tests24/Iteration 1/system acceptance test cases/Mixed/1/first_run.bat
+++ Team24/Tests24/Iteration 1/system acceptance test cases/Mixed/1/first_run.bat
@@ -1,7 +1,7 @@
 @echo off
 title Running first system acceptance test case!
 
-..\..\Code24\Debug\AutoTester.exe first_source.txt first_queries.txt out-fist_test.xml
+..\..\..\..\..\Code24\Debug\AutoTester.exe first_source.txt first_queries.txt out-fist_test.xml
 
 echo The End!
 pause
---
+++ Team24/Tests24/Iteration 1/system acceptance test cases/Uses/3/yida_run_test_2t.bat
@@ -0,0 +1,8 @@
+@echo off
+title Running yida system acceptance test case!
+
+..\..\..\..\..\Code24\Debug\AutoTester.exe yida_test_source2.txt yida_test_query2t.txt out-test_2t.xml
+
+echo The End!
+pause
+exit
