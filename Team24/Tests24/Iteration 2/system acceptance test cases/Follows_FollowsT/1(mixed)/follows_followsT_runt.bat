5e850cc5c672dc338f50c07f766c69808dcf2cd3
kohyida1997 <kohyida1997@gmail.com>
2021-10-04 20:29:14 +0800

    Return BOOLEAN or Tuples from SelectCl

--- Team24/Tests24/Iteration 2/system acceptance test cases/Follows_FollowsT/1(mixed)/follows_followsT_runt.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Running follows-FollowsT system acceptance test case!
-
-..\..\..\..\..\Code24\Debug\AutoTester.exe follows_followsT_source.txt follows_followsT_queryt.txt output.xml
-
-echo The End!
-pause
-exit
--- Team24/Tests24/Iteration 2/system acceptance test cases/Modifies/1/yida_1_run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Running yida system acceptance test case!
-
-..\..\..\..\..\Code24\Debug\AutoTester.exe yida_test_source1.txt yida_modifies_test_query1.txt out-yida_1.xml
-
-echo The End!
-pause
-exit
