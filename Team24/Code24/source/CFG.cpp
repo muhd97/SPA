945cae228b59b3d0b563c128788f3191392e4a4a
jiachen247 <jiachen247@gmail.com>
2021-10-08 11:44:26 +0800

    [CFG] Add format to cfg

--- Team24/Code24/source/CFG.cpp
+++ Team24/Code24/source/CFG.cpp
@@ -1,19 +1,63 @@
+#include "CFG.h"
 
-#include "CFG.h";
 using namespace std;
 
-string BasicBlock::format() {
-	return "todo fill in";
+int currentId = 0;
+int getNextBBId() {
+	return currentId++;
 }
 
 shared_ptr<BasicBlock> CFG::getCFG(string procName) {
-	return make_shared<BasicBlock>();
+	auto root = roots.find(procName);
+	if (root != roots.end())
+		return root->second;
+	else
+		return NULL;
 }
+
+string BasicBlock::format() {
+	string builder = "BB" + to_string(id) + "\n  ";
+	for (shared_ptr<Statement> statement : statements) {
+		builder += statement->getIndex() + " / ";
+	}
+	return builder;
+
+}
+
+
 string CFG::format() {
-	return "todo fill in";
+	string builder = " === PRINITNG CFG ===\n";
+
+	for (pair<string, shared_ptr<BasicBlock>> root : roots)
+	{
+		shared_ptr<BasicBlock> curr = root.second;
+		builder += "Blocks for procedure: " + root.first + "\n";
+		while (curr != NULL) {
+			builder += curr->format() + "\n";
+		}
+		builder += "\n";
+	}
+	return builder + "\n";
+}
+
+shared_ptr<BasicBlock> buildProcCFG(shared_ptr<Procedure> procedure) {
+	return make_shared<BasicBlock>(0);
+}
+
+shared_ptr<BasicBlock> buildIfCFG(shared_ptr<IfStatement> ifStatement, shared_ptr<BasicBlock> previous) {
+	return make_shared<BasicBlock>(0);
+}
+
+shared_ptr<BasicBlock> buildWhileCFG(shared_ptr<WhileStatement> whileStatement, shared_ptr<BasicBlock> previous) {
+	return make_shared<BasicBlock>(0);
 }
 
 shared_ptr<CFG> buildCFG(shared_ptr<Program> root) {
-	return make_shared<CFG>();
+	unordered_map<string, shared_ptr<BasicBlock>> roots;
+	for (auto proc : root->getProcedures()) {
+		roots.insert({ proc->getName(), buildProcCFG(proc) });
+	}
+
+	return make_shared<CFG>(roots);
 }
 
--- Team24/Code24/source/CFG.h
+++ Team24/Code24/source/CFG.h
@@ -1,19 +1,32 @@
 #pragma once
 #include "SimpleAST.h"
-
+#include <unordered_map>
 #include <iostream>
 
 
 using namespace std;
 
 class BasicBlock {
+private:
+	int id;
+	vector<shared_ptr<Statement>> statements;
+	// NULL if sink BB (no optional in our version of c++)
+	shared_ptr<BasicBlock> next;
 public:
+	BasicBlock(int id) {
+		this->id = id;
+	}
 	// Todo add access methods
 	string format();
 };
 
 class CFG {
+private:
+	unordered_map<string, shared_ptr<BasicBlock>> roots;
 public:
+	CFG(unordered_map<string, shared_ptr<BasicBlock>> roots) {
+		this->roots = roots;
+	}
 	shared_ptr<BasicBlock> getCFG(string procName);
 	string format();
 };
