7da358222aea73903c7a1208e172a0477c94fe4e
nicholasnge <59093518+nicholasnge@users.noreply.github.com>
2021-10-18 21:48:13 +0800

    fix follow progline

--- Team24/Code24/source/PQLProcessor.cpp
+++ Team24/Code24/source/PQLProcessor.cpp
@@ -1458,7 +1458,7 @@ void PQLProcessor::handleSuchThatClause(shared_ptr<SelectCl>& selectCl, shared_p
             if (!givenSynonymMatchesMultipleTypes(selectCl, leftSynonymKey,
                 { DesignEntity::ASSIGN, DesignEntity::CALL, DesignEntity::IF,
                  DesignEntity::PRINT, DesignEntity::READ, DesignEntity::STMT,
-                 DesignEntity::WHILE }))
+                 DesignEntity::WHILE, DesignEntity::PROG_LINE }))
             {
                 throw "Follows Error: The synonyms in a Follows relationship "
                     "must be "
@@ -1491,7 +1491,7 @@ void PQLProcessor::handleSuchThatClause(shared_ptr<SelectCl>& selectCl, shared_p
                 if (!givenSynonymMatchesMultipleTypes(selectCl, rightSynonymKey,
                     { DesignEntity::ASSIGN, DesignEntity::CALL, DesignEntity::IF,
                      DesignEntity::PRINT, DesignEntity::READ, DesignEntity::STMT,
-                     DesignEntity::WHILE }))
+                     DesignEntity::WHILE, DesignEntity::PROG_LINE }))
                 {
                     throw "Follows Error: The synonyms in a Follows "
                         "relationship must "
