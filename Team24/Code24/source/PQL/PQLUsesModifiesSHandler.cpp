597df23e032a841cd0a789494751f7ef862981cb
Manas Vegi <manasvegi@gmail.com>
2021-10-29 20:25:39 +0800

    Implement L1 UsesModifiesSHandler

---
+++ Team24/Code24/source/PQL/PQLUsesModifiesSHandler.cpp
@@ -0,0 +1,56 @@
+#include "PQLUsesModifiesSHandler.h"
+
+void UsesModifiesSHandler::validateArguments() {
+	validateStmtRef(getLeftArg(), getRelationshipType());
+	validateVarEntRef(getRightArg(), getRelationshipType());
+}
+
+shared_ptr<StmtRef>& UsesModifiesSHandler::getLeftArg()
+{
+	return leftArg;
+}
+
+shared_ptr<EntRef>& UsesModifiesSHandler::getRightArg()
+{
+	return rightArg;
+}
+
+UsesModifiesSHandler::UsesModifiesSHandler(shared_ptr<PKBPQLEvaluator>& evaluator, shared_ptr<SelectCl>& selectCl, shared_ptr<StmtRef> leftArg, shared_ptr<EntRef> rightArg)
+	: SuchThatHandler(move(evaluator), move(selectCl))
+{
+	this->leftArg = leftArg;
+	this->rightArg = rightArg;
+}
+
+void UsesModifiesSHandler::evaluate(shared_ptr<SelectCl>& selectCl, vector<shared_ptr<ResultTuple>>& toReturn)
+{
+	validateArguments();
+
+	StmtRefType leftType = leftArg->getStmtRefType();
+	EntRefType rightType = rightArg->getEntRefType();
+
+	if (leftType == StmtRefType::INTEGER && rightType == EntRefType::IDENT) {
+		evaluateIntIdent(move(toReturn));
+	}
+	else if (leftType == StmtRefType::INTEGER && rightType == EntRefType::SYNONYM) {
+		evaluateIntSyn(move(toReturn));
+	}
+	else if (leftType == StmtRefType::INTEGER && rightType == EntRefType::UNDERSCORE) {
+		evaluateIntUnderscore(move(toReturn));
+	}
+	else if (leftType == StmtRefType::SYNONYM && rightType == EntRefType::IDENT) {
+		evaluateSynIdent(move(toReturn));
+	}
+	else if (leftType == StmtRefType::SYNONYM && rightType == EntRefType::SYNONYM) {
+		evaluateSynSyn(move(toReturn));
+	}
+	else if (leftType == StmtRefType::SYNONYM && rightType == EntRefType::UNDERSCORE) {
+		evaluateSynUnderscore(move(toReturn));
+	}
+	else if (leftType == StmtRefType::UNDERSCORE) {
+		throw getRelationshipType() + " must NOT have '_' as the first argument!";
+	}
+	else {
+		throw "All 9 cases for UsesS/ModifiesS are being rejected!";
+	}
+}
---
+++ Team24/Code24/source/PQL/PQLUsesModifiesSHandler.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "PQLSuchThatHandler.h"
+#include "PQLResult.h"
+
+using namespace std;
+
+class UsesModifiesSHandler : public SuchThatHandler
+{
+private:
+	shared_ptr<StmtRef> leftArg;
+	shared_ptr<EntRef> rightArg;
+
+protected:
+	//handle all the validation cases for
+	void validateArguments() override;
+
+	/* 9 possible cases shared by UsesS and ModifiesS. */
+	virtual void evaluateIntIdent(vector<shared_ptr<ResultTuple>>& toReturn);
+	virtual void evaluateIntSyn(vector<shared_ptr<ResultTuple>>& toReturn);
+	virtual void evaluateIntUnderscore(vector<shared_ptr<ResultTuple>>& toReturn);
+	virtual void evaluateSynIdent(vector<shared_ptr<ResultTuple>>& toReturn);
+	virtual void evaluateSynUnderscore(vector<shared_ptr<ResultTuple>>& toReturn);
+	virtual void evaluateSynSyn(vector<shared_ptr<ResultTuple>>& toReturn);
+
+	shared_ptr<StmtRef>& getLeftArg();
+	shared_ptr<EntRef>& getRightArg();
+
+	UsesModifiesSHandler(shared_ptr<PKBPQLEvaluator>& evaluator, shared_ptr<SelectCl>& selectCl, shared_ptr<StmtRef> leftArg, shared_ptr<EntRef> rightArg);
+
+public:
+	void evaluate(shared_ptr<SelectCl>& selectCl, vector<shared_ptr<ResultTuple>>& toReturn) override;
+
+};
