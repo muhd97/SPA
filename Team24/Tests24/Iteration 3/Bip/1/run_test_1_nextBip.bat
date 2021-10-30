84b820842e047a90343641171cf13f98c0660390
muhd97 <zain.sma.mz@gmail.com>
2021-10-30 23:45:51 +0800

    Add more queries for bip extensions

--- Team24/Tests24/Iteration 3/Bip/1/run_test_1_nextBip.bat
+++ Team24/Tests24/Iteration 3/Bip/1/run_test_1_nextBip.bat
@@ -1,13 +1,13 @@
 @echo off
 title Running calls system acceptance test case!
 
-..\..\..\..\..\Code24\Debug\AutoTester.exe source.txt nextBip_queries.txt out-test_nextBip.xml
+..\..\..\..\..\Code24\Debug\AutoTester.exe source.txt nextBip_BipT_queries.txt out-test_nextBip_BipT.xml
 
 
-findstr "failed" out-test_nextBip.xml
-findstr "crash" out-test_nextBip.xml
-findstr "missing" out-test_nextBip.xml
-findstr "additional" out-test_nextBip.xml
+findstr "failed" out-test_nextBip_BipT.xml
+findstr "crash" out-test_nextBip_BipT.xml
+findstr "missing" out-test_nextBip_BipT.xml
+findstr "additional" out-test_nextBip_BipT.xml
 
 echo The End!
 pause
