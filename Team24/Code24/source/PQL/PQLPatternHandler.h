06c967147fb5999d59effab3f780429bc61e51c2
muhd97 <zain.sma.mz@gmail.com>
2021-10-31 23:40:26 +0800

    #35 Done Pattern Handler

--- Team24/Code24/source/PQL/PQLPatternHandler.h
+++ Team24/Code24/source/PQL/PQLPatternHandler.h
@@ -13,16 +13,16 @@ class PatternHandler : public ClauseHandler
 private:
 
 	const shared_ptr<PatternCl>& patternCl;
+	int assignment; // 1 means check validation for assignment
 
 	void validateArguments() override;
-	void validateArguments(int mode, int w) ;
 	void evaluateAssign(const string& synonymType, bool& retflag, vector<shared_ptr<ResultTuple>>& toReturn);
 	void evaluateWhileAndIf(const string& DesignEntityType, vector<shared_ptr<ResultTuple>>& toReturn);
 
 public:
 	PatternHandler(shared_ptr<PKBPQLEvaluator> evaluator, shared_ptr<SelectCl>& selectCl, const shared_ptr<PatternCl>& patternCl) : ClauseHandler(evaluator, move(selectCl)), patternCl(move(patternCl))
 	{
-		
+		this->assignment = 0;
 	}
 	void evaluate(vector<shared_ptr<ResultTuple>>& toReturn) override;
 };
