cfd9d77634b7769928ca3a38c62dc44ddcd89ccd
Zain <59967369+muhd97@users.noreply.github.com>
2021-11-01 23:50:57 +0800

    Update run_test_1_affectsBip.bat

--- Team24/Tests24/Iteration 3/Bip/1/run_test_1_affectsBip.bat
+++ Team24/Tests24/Iteration 3/Bip/1/run_test_1_affectsBip.bat
@@ -1,13 +1,13 @@
 @echo off
 title Running calls system acceptance test case!
 
-..\..\..\..\..\Code24\Debug\AutoTester.exe source.txt affectsBip_BipT_queries.txt out-test_affectsBip_BipT.xml
+..\..\..\..\Code24\Debug\AutoTester.exe source.txt affectsBip_BipT_queries.txt out-test_affectsBip_BipT.xml
 
 
 findstr "failed" out-test_affectsBip_BipT.xml
 findstr "crash" out-test_affectsBip_BipT.xml
-findstr "missing" out-test_affectsBip_BipT.xml
-findstr "additional" out-test_affectsBip_BipT.xml
+findstr "exception" out-test_affectsBip_BipT.xml
+findstr "timeout" out-test_affectsBip_BipT.xml
 
 echo The End!
 pause
