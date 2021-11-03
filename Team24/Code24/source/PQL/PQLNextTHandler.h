cd26a58e021ffe65b4a5910936dca4685b6004f1
Manas Vegi <manasvegi@gmail.com>
2021-11-04 00:44:27 +0800

    Implement NextTHandler

---
+++ Team24/Code24/source/PQL/PQLNextTHandler.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "PQLFollowsParentNextHandler.h"
+
+class NextTHandler : public FollowsParentNextHandler
+{
+private:
+	//use override method well to ensure OOP sanity
+	void evaluateIntInt(vector<shared_ptr<ResultTuple>>& toReturn) override;
+	void evaluateIntSyn(vector<shared_ptr<ResultTuple>>& toReturn) override;
+	void evaluateIntUnderscore(vector<shared_ptr<ResultTuple>>& toReturn) override;
+	void evaluateSynInt(vector<shared_ptr<ResultTuple>>& toReturn) override;
+	void evaluateSynUnderscore(vector<shared_ptr<ResultTuple>>& toReturn) override;
+	void evaluateSynSyn(vector<shared_ptr<ResultTuple>>& toReturn) override;
+	void evaluateUnderscoreSyn(vector<shared_ptr<ResultTuple>>& toReturn) override;
+	void evaluateUnderscoreInt(vector<shared_ptr<ResultTuple>>& toReturn) override;
+	void evaluateUnderscoreUnderscore(vector<shared_ptr<ResultTuple>>& toReturn) override;
+
+	const string& getRelationshipType() override;
+
+public:
+	NextTHandler(shared_ptr<PKBPQLEvaluator>& evaluator, shared_ptr<SelectCl>& selectCl, shared_ptr<NextT>& nextTCl);
+};
