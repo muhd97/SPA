e2a074d10c5f1f78567a9e23a3c2336c5df50593
muhd97 <zain.sma.mz@gmail.com>
2021-11-05 14:06:29 +0800

    #28 Remove unused codes #30 Update some test scripts to use Release config

--- Team24/Tests24/Iteration 2/system acceptance test cases/Uses Tuples Boolean/2/yida_run_test_1a.bat
+++ Team24/Tests24/Iteration 2/system acceptance test cases/Uses Tuples Boolean/2/yida_run_test_1a.bat
@@ -1,7 +1,7 @@
 @echo off
 title Running yida system acceptance test case!
 
-..\..\..\..\..\Code24\Debug\AutoTester.exe yida_test_source1.txt yida_test_query1a.txt out-test_1a.xml
+..\..\..\..\..\Code24\Release\AutoTester.exe yida_test_source1.txt yida_test_query1a.txt out-test_1a.xml
 
 
 findstr "failed" out-test_1a.xml
